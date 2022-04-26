4d7e2b6f347a8074c0bc677ad7af0113d2903a9c
MIkaela Braga <bragamikaela@gmail.com>
2022-04-25 22:41:54 -0300

    feat: implements Car Interface

--- src/interfaces/ModelInterface.ts
+++ src/interfaces/ModelInterface.ts
@@ -1,7 +1,7 @@
 export interface Model {
   create(object: T): Promise<T>,
-  read(): Promise<T>[],
+  read(): Promise<T[]>,
   readOnde(id: string): Promise<T>,
-  update(id: string): Promise<T> | null,
-  delete(id: string): Promise<T> | null
+  update(id: string): Promise<T | null>,
+  delete(id: string): Promise< | null>,
 }
